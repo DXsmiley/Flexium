49c37d3df29044b7e2f5006c2b05ed8607dafde6
DXsmiley <dxsmiley@hotmail.com>
2014-12-30 18:20:26 +1100

    Delete Flexium.d

--- obj/Flexium.d
+++
@@ -1,5 +0,0 @@
-obj/Flexium.o: src/Flexium.cpp include/Flexium/Flexium.hpp \
- include/Flexium/Vector.hpp include/Flexium/Console.hpp \
- include/Flexium/ConsoleMinimal.hpp include/Flexium/Object.hpp \
- include/Flexium/Sprite.hpp include/Flexium/Window.hpp \
- include/Flexium/Input.hpp
