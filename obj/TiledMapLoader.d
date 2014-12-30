d46f56e4d0823d8e18777bb5414490cafd77f6b0
DXsmiley <dxsmiley@hotmail.com>
2014-12-30 18:20:57 +1100

    Delete TiledMapLoader.d

--- obj/TiledMapLoader.d
+++
@@ -1,6 +0,0 @@
-obj/TiledMapLoader.o: src/TiledMapLoader.cpp \
- include/Flexium/TiledMapLoader.hpp include/Flexium/Flexium.hpp \
- include/Flexium/Vector.hpp include/Flexium/pugixml.hpp \
- include/Flexium/pugiconfig.hpp include/Flexium/Tilemap.hpp \
- include/Flexium/Object.hpp include/Flexium/Console.hpp \
- include/Flexium/ConsoleMinimal.hpp include/Flexium/World.hpp
