1428a81331280b7c98b9610979699f6415b41189
DXsmiley <dxsmiley@hotmail.com>
2014-12-30 18:20:45 +1100

    Delete Sprite.d

--- obj/Sprite.d
+++
@@ -1,5 +0,0 @@
-obj/Sprite.o: src/Sprite.cpp include/Flexium/Sprite.hpp \
- include/Flexium/Window.hpp include/Flexium/Flexium.hpp \
- include/Flexium/Vector.hpp include/Flexium/Console.hpp \
- include/Flexium/ConsoleMinimal.hpp include/Flexium/Object.hpp \
- include/Flexium/pugixml.hpp include/Flexium/pugiconfig.hpp
