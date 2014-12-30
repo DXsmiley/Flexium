47cc04b5f18fda7d8436e9e69f2fe84f30d766c3
DXsmiley <dxsmiley@hotmail.com>
2014-12-30 18:20:30 +1100

    Delete FlexiumUtility.d

--- obj/FlexiumUtility.d
+++
@@ -1,8 +0,0 @@
-obj/FlexiumUtility.o: src/FlexiumUtility.cpp \
- include/Flexium/FlexiumUtility.hpp include/Flexium/Flexium.hpp \
- include/Flexium/Vector.hpp include/Flexium/Object.hpp \
- include/Flexium/Event.hpp include/Flexium/Timer.hpp \
- include/Flexium/Console.hpp include/Flexium/ConsoleMinimal.hpp \
- include/Flexium/Input.hpp include/Flexium/Sprite.hpp \
- include/Flexium/Window.hpp include/Flexium/pugixml.hpp \
- include/Flexium/pugiconfig.hpp
