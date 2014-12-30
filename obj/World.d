6108d4c9dc529e95d97053bce26ccca6e52b1f7f
DXsmiley <dxsmiley@hotmail.com>
2014-12-30 18:21:13 +1100

    Delete World.d

--- obj/World.d
+++
@@ -1,5 +0,0 @@
-obj/World.o: src/World.cpp include/Flexium/World.hpp \
- include/Flexium/Object.hpp include/Flexium/Flexium.hpp \
- include/Flexium/Vector.hpp include/Flexium/Window.hpp \
- include/Flexium/Input.hpp include/Flexium/Console.hpp \
- include/Flexium/ConsoleMinimal.hpp
