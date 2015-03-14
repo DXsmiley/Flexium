3ae91ddfdf93295fa5cc4d0c9c1b3a6d22199598
DXsmiley <dxsmiley@hotmail.com>
2015-03-14 18:47:22 +1100

    Project template Main.cpp now includes Debugger.hpp
    As opposed to FlexiumUtility.hpp
    This is because it does not need all the content of FlexiumUtility.hpp

--- utility/template/src/Main.cpp
+++ utility/template/src/Main.cpp
@@ -1,22 +1,34 @@
+/*
+
+	This is a simple main file for starting your own project from.
+
+
+*/
+
 #include <Flexium/World.hpp>
 #include <Flexium/Window.hpp>
 #include <Flexium/Sprite.hpp>
 #include <Flexium/Audio.hpp>
 #include <Flexium/Text.hpp>
 #include <Flexium/Console.hpp>
-#include <Flexium/FlexiumUtility.hpp>
+#include <Flexium/Debugger.hpp>
 
 int main() {
 
+	// Create the window
 	flx::Window::WindowSettings ws;
 	ws.title = "Window Title";
 	flx::Window::setClearColor(sf::Color::White);
 	flx::Window::initiate(ws);
 
+	// Load resources
 	flx::Sprite::loadMapping("spritesheet.xml");
 	flx::Audio::loadMapping("soundsheet.xml");
 	flx::Text::loadMapping("fontsheet.xml");
 
+	// Setup the world
+	// When you release your game, you should disable the debugger
+	// and in-game console.
 	flx::World world;
 	world.instanceAdd(new flx::Debugger());
 	world.instanceAdd(new flx::ConsoleInGame());
